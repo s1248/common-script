444dcf3b72058592af5a9e43c997a640b8e97542
s1248.com <76787070+s1248@users.noreply.github.com>
2024-07-07 05:40:46 +0700

    Update open-ssh.sh

--- open-ssh.sh
+++ open-ssh.sh
@@ -5,6 +5,6 @@ sudo ufw status verbose
 sudo apt-get install openssh-server -y
 sudo systemctl enable ssh --now
 sudo systemctl start ssh
-mkdir -p ~/.ssh
-touch ~/.ssh/authorized_keys
+sudo mkdir -p ~/.ssh
+sudo touch ~/.ssh/authorized_keys
 sudo apt autoremove -y
