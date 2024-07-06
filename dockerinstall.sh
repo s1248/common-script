60c6912484b08ebab8ed3bfdc6cae0e920ce8da2
s1248.com <76787070+s1248@users.noreply.github.com>
2024-07-07 03:07:45 +0700

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -11,4 +11,4 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
