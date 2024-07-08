70ba79ccb77b8746acb082d6d66dc44585814c44
s1248.com <76787070+s1248@users.noreply.github.com>
2024-07-09 03:03:15 +0700

    Update convert-ubuntu-desktop-to-server.sh

--- convert-ubuntu-desktop-to-server.sh
+++ convert-ubuntu-desktop-to-server.sh
@@ -1,4 +1,4 @@
 sudo apt install ubuntu-server -y
 sudo systemctl set-default multi-user.target
-sudo apt purge ubuntu-desktop -y && sudo apt autoremove -y && sudo apt autoclean
+sudo apt purge ubuntu-desktop -y && sudo apt autoremove -y && sudo apt autoclean -y
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
